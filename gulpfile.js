d0ef83faf143e20fb36789b2f45b5cc28f9c61d3
Lednev <iLednevii@gmail.com>
2020-02-20 14:42:06 +0300

    Начат header (2 недели назад)

--- gulpfile.js
+++ gulpfile.js
@@ -105,7 +105,7 @@ gulp.task('server', function () {
   gulp.watch('source/js/**/*.js', gulp.series('js'));
   gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
   gulp.watch('source/img/svg/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
-  gulp.watch('source/*.html', gulp.series('html', 'refresh'));
+  gulp.watch('source/**/*.html', gulp.series('html', 'refresh'));
 });
 
 gulp.task('refresh', function (done) {
